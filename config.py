d545ac0c4a150fd16e7180afcfce90fb4a57abe0
juancamilo <jcvalencia22@hotmail.com>
2018-06-20 22:05:03 -0500

    Modify config.py and run.py and create test.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# The configuration settings are defined as class variables inside the Config class. 
+# As the application needs more configuration items, they can be added to this class, 
+# and later if I find that I need to have more than one configuration set, 
+# I can create subclasses of it. 
+class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	SQLALCHEMY_DATABASE_URI = 'mysql://juancamilo:4454861@localhost/practiflask'							#mysql://username:password@server/db
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
